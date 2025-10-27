3d773c6ed58af70be0a078d90a939feba5fc1cc7
rtsh13 <40361131+rtsh13@users.noreply.github.com>
2025-10-27 13:37:56 +0000

    common functions

---
+++ pkg/utils/common.go
@@ -0,0 +1,78 @@
+package utils
+
+import "time"
+
+// File metadata
+type FileMetadata struct {
+	Filename     string
+	Size         int64
+	Version      int64
+	LastModified time.Time
+}
+
+// RPC Request/Response structures
+type OpenRequest struct {
+	ClientID string
+	Filename string
+	Mode     string // "r", "w", "rw"
+}
+
+type OpenResponse struct {
+	Success  bool
+	Metadata FileMetadata
+	Error    string
+}
+
+type FetchFileRequest struct {
+	ClientID string
+	Filename string
+}
+
+type FetchFileResponse struct {
+	Success bool
+	Content []byte
+	Version int64
+	Error   string
+}
+
+type TestAuthRequest struct {
+	ClientID string
+	Filename string
+	Version  int64
+}
+
+type TestAuthResponse struct {
+	Valid   bool
+	Version int64
+}
+
+type StoreFileRequest struct {
+	ClientID string
+	Filename string
+	Content  []byte
+}
+
+type StoreFileResponse struct {
+	Success    bool
+	NewVersion int64
+	Error      string
+}
+
+type CreateFileRequest struct {
+	ClientID string
+	Filename string
+}
+
+type CreateFileResponse struct {
+	Success bool
+	Error   string
+}
+
+type GetDirectoriesRequest struct {
+	ClientID string
+}
+
+type GetDirectoriesResponse struct {
+	InputDir  string
+	OutputDir string
+}
