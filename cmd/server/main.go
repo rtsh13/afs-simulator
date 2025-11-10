490d0a0041c6a8d50f3561522f3dadf03e0a1485
rtsh13 <40361131+rtsh13@users.noreply.github.com>
2025-11-10 07:10:34 +0000

    capture replica addresses and start a replicated server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,9 +12,29 @@ func main() {
 	inputDir := flag.String("input", "data/input", "Input directory path")
 	outputDir := flag.String("output", "data/output", "Output directory path")
 	address := flag.String("addr", ":8080", "Server address")
+
+	id := flag.String("id", "server-default", "Unique Server ID")
+	replicas := flag.String("replicas", "", "Comma-separated list of OTHER replica addresses (exclude self)")
+	isPrimaryFlag := flag.Bool("primary", false, "Start this server as the initial primary")
+
 	flag.Parse()
 
-	server, err := afs.NewFileServer(*inputDir, *outputDir)
+	// string commas to get the list of replicas list
+	replicaAddrs := []string{}
+	if *replicas != "" {
+		parts := strings.Split(*replicas, ",")
+		for _, addr := range parts {
+			trimmed := strings.TrimSpace(addr)
+			if trimmed != "" {
+				replicaAddrs = append(replicaAddrs, trimmed)
+			}
+		}
+	}
+
+	log.Printf("Starting replica server %s on %s", *id, *address)
+	log.Printf("Will connect to replicas: %v", replicaAddrs)
+
+	server, err := afs.NewReplicaServer(*id, *inputDir, *outputDir, replicaAddrs)
 	if err != nil {
 		log.Fatalf("Failed to create server: %v", err)
 	}
